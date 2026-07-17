7cea63ba7371a3c6d154bb6a74892d1d935414a9
mhdibrahimcn <mhdcrew46@gmail.com>
2026-07-17 07:52:52 +0530

    chore: bump to v0.4.3

--- Formula/droploid.rb
+++ Formula/droploid.rb
@@ -1,8 +1,8 @@
 class Droploid < Formula
   desc "CLI to deploy Flutter & native iOS/Android apps to the App Store & Play Store"
   homepage "https://github.com/mhdibrahimcn/droploid-cli"
-  url "https://github.com/mhdibrahimcn/Droploid-_electron/archive/refs/tags/v0.4.2.tar.gz"
-  sha256 "3abadb9c458b8b2b17eed3188f9b9913a09ff02bcd8fe230cea74a4ee152e0c0"
+  url "https://github.com/mhdibrahimcn/Droploid-_electron/archive/refs/tags/v0.4.3.tar.gz"
+  sha256 "ec319e763aa7f86f505e35efc5b5e7104310b41dde775ac41e2cbeefbccc0f77"
   license "MIT"
 
   depends_on "node"
